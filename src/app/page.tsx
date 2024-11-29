2ac4aeced306b69eed347c308a0a77fbcfd13110
Andres Felipe Lopez Taborda <lopez920913@hotmail.com>
2024-11-29 14:29:58 -0500

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import database from '../helpers/products.json'
 import { fCurrency } from "./utils/formatNumber";
 import { useRouter } from 'next/navigation';
 
-export const runtime = "nodejs"
+export const runtime = "edge"
 
 export default function Home() {
 
